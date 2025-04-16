a66eb2592bfee1cbe81182b30b8e50ec12aa0e83
Perera K C G S <it21810664@my.sliit.lk>
2025-04-16 14:58:02 +0530

    clean the app.js in user-service by removing unnessasary commented codes.

--- backend/user-service/app.js
+++ backend/user-service/app.js
@@ -4,17 +4,9 @@ const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
-// Additions
-// const router = express.Router();
-// const { updateUserById } = require("./controllers/userController");
-
-
 // Middleware
 app.use(cors());
-
 app.use(express.json());
-// app.use(express.json({ limit: '10mb' }));
-// app.use(express.urlencoded({ extended: true }));
 
 // ✅ Log every incoming request
 app.use((req, res, next) => {
@@ -25,7 +17,6 @@ app.use((req, res, next) => {
 
 // Routes
 app.use('/user-service', userRoutes);
-// router.put("/:id", updateUserById);
 
 
 // ✅ Global error handler – MUST be after all routes and middleware
